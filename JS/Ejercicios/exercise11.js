6e983684bf03744da32c4b1b9a56656bac4cd658
SrWilly19 <wahc1998@gmail.com>
2023-08-30 19:42:10 +0200

    ejercicio11 completo

--- JS/Ejercicios/exercise11.js
+++ JS/Ejercicios/exercise11.js
@@ -10,11 +10,11 @@ false === 0
 4 <= 2
 4 >= 8
 */
-
-10 < 11
-45 == 45
-true === true
-false === false
-1 == 1
-4 <= 6
-4 >= 8
+// Utilizamos el console.log para imprimir por pantalla y verificar el resultado.
+console.log(10 < 11)
+console.log(45 == 45)
+console.log(true === true)
+console.log(false === false)
+console.log(1 == 1)
+console.log(4 <= 6)
+console.log(4 >= 4)
