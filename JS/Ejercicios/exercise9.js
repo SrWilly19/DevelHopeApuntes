21ec8d25338b0be3535d43bb7fee5e800dd90541
SrWilly19 <wahc1998@gmail.com>
2023-08-30 13:50:53 +0200

    Hasta el 9

---
+++ JS/Ejercicios/exercise9.js
@@ -0,0 +1,21 @@
+/*# Exercise 9
+
+* Try to predict the output of the comparisons before printing them in the console
+*/
+
+const name = "Mario";
+const lastname = "Rossi";
+const age = 27;
+const isGraduated = false;
+const average = 27;
+const examsCompleted = 10;
+const firstYearCompleted = true;
+const yearsCompleted = 1;
+
+console.log(name == lastname) //false
+age <= average //true
+firstYearCompleted == lastname //false
+yearsCompleted == firstYearCompleted //true
+firstYearCompleted === yearsCompleted  //false
+examsCompleted < age //true
+isGraduated > yearsCompleted //false
--- JS/apuntes.js
+++ JS/apuntes.js
@@ -93,3 +93,148 @@ let trueValue2 = true;
 let trueString = "true";
 let falsevalue = false; 
 console.log(Boolean(0));
+
+//truth-y and false-y
+// str -> String
+//console.log(str == trueValue)
+//console.log(emptyString == true)
+console.log(Boolean(1));
+console.log(Boolean(1) == true);
+
+//assignment
+let bool = true;
+//comparison (Convierte los tipos y luego los campara entre si para ver si es verdadero o falso)
+//equality
+bool == true;
+//greater than
+bool  > true;
+0 > 1
+Number > Number;
+
+//Strings vs Numbers (Con los 3= lo que hara sera comparar todo, incluso el typo de variable que es, si son iguales dara true. )
+let right = 1;
+console.log(str === right);
+
+
+// Ejercicios!!!!!
+
+//Ejercicio edad promedio
+
+let edad1 = 25;
+let edad2 = 19;
+let edad3 = 35;
+
+let promedio = (edad1+ edad2 + edad3 )/3
+//console.log(promedio)
+
+//Convierte la temperatura de grados celsius a fahrenheit.
+
+let temperaturaCelsius = 25;
+let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+
+console.log("La temperatura en Fahrenheit es: ", temperaturaFahrenheit);
+
+//Calcular un perimetro de un rectangulo 
+let anchoRec = 8, largoRec = 12;
+let perimetroRec = 2 * (anchoRec + largoRec);
+
+console.log(perimetroRec);
+
+//Comparaciones
+const numero1 = 15, numero2 = 10;
+
+if(numero1 > numero2){
+    console.log("El primer numero es mayor.");
+}else if (numero1 > numero2){
+    console.log("El primer numero es menor.");
+}else{
+    console.log("Los numeros son iguales.")
+}
+
+//Calcular el precio final de un producto despues de aplicarle un descuento
+let precioOriginal = 100;
+let porcentajeDescuento = 20;
+
+let precioFinal= precioOriginal - (precioOriginal * (porcentajeDescuento / 100))
+
+console.log("El precio final con descuento es: ", precioFinal);
+
+//Determina si una persona es elegible para votar en base a su edad y ciudadania.
+
+const edad = 22;
+const esCiudadano = true;
+
+if(edad >= 18 && esCiudadano){
+    console.log("Puede votar");
+}else{
+    console.log("No puede votar");
+}
+
+//Permite el acceso a una pelicula si la persona es mayor de 13 años o esta acompañada por un  adulto
+let edadPersona = 15;
+let acompaAdulto = false;
+
+if (edadPersona > 13 || acompaAdulto){
+    console.log("Puede acceder.")
+}else{
+    console.log("No puede acceder.")
+}
+
+//Si una contraseña cumple los requisitos de longuitud y contenido.
+const password = "secreto123";
+
+if(password.length >= 8 && (password.includes("id:") || password.includes("description:"))){
+    console.log("Positivo");
+}else{
+    console.log("Negative");
+}
+
+//Si es fin de semana
+
+let dia = "Sabado";
+if(dia == "Sabado" || dia == "Domingo"){
+    console.log("es fin de semana")
+}else{
+    console.log("es dia laborable")
+}
+
+//Convertir un numero en formato string a tipo numerico y realizar una operacion matematica
+
+let numTexto = "20"
+let numero = Number(numTexto);
+console.log(numTexto+5); // 20
+
+let valor1 = 5;
+let valor2 = "5";
+
+if(valor1 === valor2){
+    console.log("Los valores son iguales y los tipos son iguales");
+}else if (valor1 == valor2){
+    console.log("Los valroes son iguales perop los tipos no");
+}else{
+    console.log("No son iguales");
+}
+
+//Converit una varibales de tipo booleano y la comparamos con otroa variable booleana.
+
+let valorTexto = 'true'
+let valorBooleano = Boolean(valorTexto);
+
+let otroValorBooleano = false;
+
+if(valorBooleano === otroValorBooleano){
+    console.log("Los valores son iguales y los tipos tambien");
+}else if(valorBooleano == otroValorBooleano){
+    console.log("Los tipos distintos pero el valor igual");
+}else{
+    console.log("false")
+}
+
+let valorTexto1 = "a";
+let valorBooleano1 = Boolean(valorTexto1);
+
+if(!valorBooleano){
+    console.log("false");
+}else{
+    console.log("TRUE");
+}
