bd848d3dc48f11415e38270f0de2c45ee650e007
SrWilly19 <wahc1998@gmail.com>
2023-08-30 17:39:21 +0200

    Ejercicio11

---
+++ JS/Ejercicios/exercise11.js
@@ -0,0 +1,20 @@
+/*# Exercise 11
+
+Modify the second term of the expression in order to get true as result.
+
+10 < 9
+45 == 34
+true === 1
+false === 0
+1 == 0
+4 <= 2
+4 >= 8
+*/
+
+10 < 11
+45 == 45
+true === true
+false === false
+1 == 1
+4 <= 6
+4 >= 8
