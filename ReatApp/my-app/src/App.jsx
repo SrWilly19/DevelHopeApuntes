ced39b9cbb2c9d7ddc2e815a0fb17547ce8354b4
SrWilly19 <wahc1998@gmail.com>
2023-12-15 23:04:14 +0100

    apuntes jsx

--- ReatApp/my-app/src/App.jsx
+++ ReatApp/my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { AlertClock } from "./AlertClock"
+import { Counter } from "./Counter"
 import { HelloWorld } from "./HelloWorld"
 import { Welcome } from "./Welcome"
 
@@ -11,6 +12,7 @@ export function App(){
             <hr />
             <Welcome name="Arnau" />
             <AlertClock />
+            <Counter />
         </div>
     )
 }
